e5005b11cf9dec96afb69fac19a2cbd5313e54c4
MeharCreation <143766015+MeharCreation@users.noreply.github.com>
2023-08-31 22:08:03 -0400

    Add files via upload

---
+++ services.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel= "stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/
+  css?family=Josefin+Sans&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+<?php include 'menu.php'?>
+<div class="jumbotron">
+  <h1>Harman</h1>
+  <p>We are the web developers</p>
+</div>
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center">Our Services</h2>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" >
+  <img class="card-img-top" src="images/machine.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Sewing and Alterations</h4>
+    <p class="card-text">Elevate your travel adventure by reserving your spot at 
+        renowned tourist hotspots through our convenient and reliable
+         booking platform.</p>
+    
+  </div>
+</div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" >
+  <img class="card-img-top" src="images/26.webp" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Stuff and Designs</h4>
+    <p class="card-text">Enjoy a smooth and comfortable journey between attractions
+         with our dedicated transit service, enhancing your tour experience.</p>
+    
+  </div>
+</div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+            <div class="card" >
+  <img class="card-img-top" src="images/offers.png" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Offers</h4>
+    <p class="card-text">Elevate your travel experience by savoring the local flavors through
+         our curated food service, offering a delightful fusion of tradition and innovation 
+         for every discerning tourist.</p>
+    
+  </div>
+</div>
+ 
+            </div>
+        </div>
+
+</div>
+</section>
+</body>
+</html>
